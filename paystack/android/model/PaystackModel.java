5afde5988686fbf4a9931772f1aee41a281341ab
ABEL AYUBA <adugamayuba@gmail.com>
2019-05-01 13:55:30 +0100

    Deleted unrequired files in repo

--- paystack/android/model/PaystackModel.java
+++
@@ -1,9 +0,0 @@
-package co.paystack.android.model;
-
-/**
- * Base class for Paystack Model classes
- *
- * @author {androidsupport@paystack.co} on 9/13/15.
- */
-public abstract class PaystackModel {
-}
